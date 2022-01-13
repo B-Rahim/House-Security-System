29b6e910c044099e5db19248e59e5405dcb09c0b
Abderrahim Benaouda <79881592+B-Rahim@users.noreply.github.com>
2022-01-13 01:26:05 +0100

    Safe 1.0

--- Embedded App/Types.h
+++ Embedded App/Types.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// constants won't change. They're used here to set pin numbers:
+// Macros used to set pin numbers:
 #define LED_PIN 13    //  LED pin
 #define PAIR_BUTTON 7 //  Bluetooth paiting button pin
 #define PWM_PIN 9     // Buzzer pin
@@ -9,7 +9,9 @@
 #define CYCLE_1 2700 // The PWM frequency will be 13MHz / 8 / (c1,c2) = (600,300)Hz
 #define CYCLE_2 5400 // to have a siren like sound
 
-//The finite state machine of system has 4 states
+//The finite state machine of the program has 4 states 
+// that define the behaviour of the system
+
 typedef enum
 {
     S_IDLE,
